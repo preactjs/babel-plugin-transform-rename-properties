6b00bae03170cf11a1388a46bb30451ee0782171
Joachim Viide <jviide@iki.fi>
2019-08-22 15:46:10 +0300

    Replace properties in object patterns and expressions

--- test/index.test.js
+++ test/index.test.js
@@ -21,42 +21,116 @@ function compare(input, output, options = {}) {
 }
 
 describe("babel-plugin-transform-rename-properties", () => {
-  it("renames only properties", () => {
-    compare(
-      `
-      function foo(obj) {
-        return obj.foo + obj.bar
-      }
-      `,
-      `
-      function foo(obj) {
-        return obj.__FOO__ + obj.__BAR__
-      }
-      `,
-      {
-        rename: {
-          foo: "__FOO__",
-          bar: "__BAR__"
+  describe("for object member access", () => {
+    it("renames only properties", () => {
+      compare(
+        `
+        function foo(obj) {
+          return obj.foo + obj.bar
         }
-      }
-    );
-  });
+        `,
+        `
+        function foo(obj) {
+          return obj.__FOO__ + obj.__BAR__
+        }
+        `,
+        {
+          rename: {
+            foo: "__FOO__",
+            bar: "__BAR__"
+          }
+        }
+      );
+    });
 
-  it("renames properties from the middle", () => {
-    compare("obj.foo.bar", "obj.__FOO__.bar", {
-      rename: { foo: "__FOO__" }
+    it("renames properties from the middle", () => {
+      compare("obj.foo.bar", "obj.__FOO__.bar", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+
+    it("turns bad identifiers to computed props", () => {
+      compare("obj.foo", "obj['bad identifier']", {
+        rename: { foo: "bad identifier" }
+      });
+    });
+
+    it("does not rename already renamed properties", () => {
+      compare("obj.foo", "obj.__FOO__", {
+        rename: { foo: "__FOO__", __FOO__: "foo" }
+      });
     });
   });
 
-  it("turns bad identifiers to computed props", () => {
-    compare("obj.foo", "obj['bad identifier']", {
-      rename: { foo: "bad identifier" }
+  describe("for object pattern properties", () => {
+    it("replaces identifiers with identifiers when possible", () => {
+      compare("const { foo: bar } = {}", "const { __FOO__: bar } = {}", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+    it("replaces identifiers with string literals when necessary ", () => {
+      compare(
+        "const { foo: bar } = {}",
+        "const { 'I HAVE SPACES': bar } = {}",
+        {
+          rename: { foo: "I HAVE SPACES" }
+        }
+      );
+    });
+    it("replaces string literals with string literals", () => {
+      compare("const { 'foo': bar } = {}", "const { '__FOO__': bar } = {}", {
+        rename: { foo: "__FOO__" }
+      });
     });
   });
 
-  it("does not rename already renamed properties", () => {
-    compare("obj.foo", "obj.__FOO__", {
-      rename: { foo: "__FOO__", __FOO__: "foo" }
+  describe("for object expression properties", () => {
+    it("replaces identifiers with identifiers when possible", () => {
+      compare("const a = { foo: bar }", "const a = { __FOO__: bar }", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+    it("replaces identifiers with string literals when necessary ", () => {
+      compare("const a = { foo: bar }", "const a = { 'I HAVE SPACES': bar }", {
+        rename: { foo: "I HAVE SPACES" }
+      });
+    });
+    it("replaces string literals with string literals", () => {
+      compare("const a = { 'foo': bar }", "const a = { '__FOO__': bar }", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+  });
+
+  describe("for object expression methods", () => {
+    it("replaces identifiers with identifiers when possible", () => {
+      compare("const a = { foo() {} }", "const a = { __FOO__() {} }", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+    it("replaces identifiers with string literals when necessary ", () => {
+      compare("const a = { foo() {} }", "const a = { 'I HAVE SPACES'() {} }", {
+        rename: { foo: "I HAVE SPACES" }
+      });
+    });
+    it("replaces string literals with string literals", () => {
+      compare("const a = { 'foo'() {} }", "const a = { '__FOO__'() {} }", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+    it("replaces getters", () => {
+      compare("const a = { get foo() {} }", "const a = { get __FOO__() {} }", {
+        rename: { foo: "__FOO__" }
+      });
+    });
+    it("replaces setters", () => {
+      compare(
+        "const a = { set foo(value) {} }",
+        "const a = { set __FOO__(value) {} }",
+        {
+          rename: { foo: "__FOO__" }
+        }
+      );
     });
   });
 });
